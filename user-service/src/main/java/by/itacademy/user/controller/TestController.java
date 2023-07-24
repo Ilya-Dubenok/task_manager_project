2201bce2af93fcd6afe9e92265a96b3b0da07170
Ilya Shadryn <i.shadryn@novacom.by>
2023-07-24 20:18:13 +0300

    change base url on TestController.java on user-service

--- user-service/src/main/java/by/itacademy/user/controller/TestController.java
+++ user-service/src/main/java/by/itacademy/user/controller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users/user")
 public class TestController {
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String test(){
