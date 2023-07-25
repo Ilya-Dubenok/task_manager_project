2bdeacf26bebba31dfa63c7ce30fc20f11f09bb1
Ilya Dubenok <dubenokilya@gmail.com>
2023-07-25 22:59:30 +0300

    added controller layer

--- audit-service/src/main/java/org/example/Main.java
+++ audit-service/src/main/java/org/example/Main.java
@@ -6,16 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableJpaRepositories
 @EnableTransactionManagement
 @EnableJpaAuditing
 @EnableConfigurationProperties
-@EnableScheduling
-@EnableAsync
 @SpringBootApplication
 public class Main {
 
---
+++ audit-service/src/main/java/org/example/endpoint/web/AuditController.java
@@ -0,0 +1,73 @@
+package org.example.endpoint.web;
+
+
+import org.example.core.dto.AuditCreateDTO;
+import org.example.core.dto.AuditDTO;
+import org.example.core.dto.PageOfTypeDTO;
+import org.example.dao.entities.audit.Audit;
+import org.example.service.api.IAuditService;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/audit")
+public class AuditController {
+
+    private IAuditService auditService;
+    private ConversionService conversionService;
+
+
+
+    public AuditController(IAuditService auditService, ConversionService conversionService) {
+        this.auditService = auditService;
+        this.conversionService = conversionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createAudit(@RequestBody AuditCreateDTO auditCreateDTO) {
+        auditService.save(auditCreateDTO);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
+    }
+
+    @GetMapping(value = "/{uuid}")
+    public ResponseEntity<AuditDTO> getUserByUuid(@PathVariable UUID uuid) {
+
+        Audit audit = auditService.getAuditById(uuid);
+        AuditDTO dto = conversionService.convert(audit, AuditDTO.class);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<PageOfTypeDTO<AuditDTO>> getPAgeOfAudit(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                        @RequestParam(value = "size", defaultValue = "20") Integer size){
+
+        Page<Audit> pageOfAudit = auditService.getPageOfAudit(page, size);
+
+        ResolvableType resolvableType = ResolvableType.forClassWithGenerics(
+                PageOfTypeDTO.class, AuditDTO.class
+        );
+
+        PageOfTypeDTO<AuditDTO> converted = (PageOfTypeDTO<AuditDTO>) conversionService.convert(
+                pageOfAudit, TypeDescriptor.valueOf(PageImpl.class),
+                new TypeDescriptor(resolvableType, null, null)
+        );
+
+        return new ResponseEntity<>(
+                converted, HttpStatus.OK
+        );
+
+
+    }
+
+
+}
