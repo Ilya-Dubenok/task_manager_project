7f1e382e25f51f39e2e22d6fa941265769506315
Ilya Dubenok <dubenokilya@gmail.com>
2023-08-02 00:39:35 +0300

    added reminder to write exception handling for 1 url

--- user-service/src/main/java/org/example/endpoint/web/exception_handler/ControllerExceptionHandler.java
+++ user-service/src/main/java/org/example/endpoint/web/exception_handler/ControllerExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -59,6 +60,11 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(List.of(generalExceptionDTO), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    //TODO write for "code"/"mail" missing params in users/verification request
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return super.handleMissingServletRequestParameter(ex, headers, status, request);
+    }
 
     @Override
     protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
