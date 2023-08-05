14ff9c7bdab3790be5082035fa405124d640efb8
Ilya Dubenok <dubenokilya@gmail.com>
2023-08-05 20:28:14 +0300

    -added basic security parts (jwt token handling, setting security config, etc.) in audit-service

--- audit-service/src/main/java/org/example/config/Config.java
+++ audit-service/src/main/java/org/example/config/Config.java
@@ -1,6 +1,7 @@
 package org.example.config;
 
 import org.example.config.property.ApplicationProperties;
+import org.example.config.property.JWTProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,13 @@ public class Config {
     @ConfigurationProperties("app")
     public ApplicationProperties getProperty() {
         return new ApplicationProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("jwt")
+    public JWTProperties getJWTProperties() {
+        return new JWTProperties();
+
     }
 
 }
---
+++ audit-service/src/main/java/org/example/utils/jwt/JwtTokenHandler.java
@@ -0,0 +1,68 @@
+package org.example.utils.jwt;
+
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.example.config.property.JWTProperties;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenHandler {
+
+    private final JWTProperties properties;
+
+    private final Key signingKey;
+
+    public JwtTokenHandler(JWTProperties properties) {
+        this.properties = properties;
+        this.signingKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(properties.getSecret()));
+    }
+
+
+
+    public String getUsername(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(signingKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+
+        return claims.getSubject();
+    }
+
+    public Date getExpirationDate(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(signingKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getExpiration();
+    }
+
+    public boolean validate(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(signingKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch ( io.jsonwebtoken.security.SignatureException ex) {
+            //logger.error("Invalid JWT signature - {}", ex.getMessage());
+        } catch (MalformedJwtException ex) {
+            //logger.error("Invalid JWT token - {}", ex.getMessage());
+        } catch (ExpiredJwtException ex) {
+            //logger.error("Expired JWT token - {}", ex.getMessage());
+        } catch (UnsupportedJwtException ex) {
+            //logger.error("Unsupported JWT token - {}", ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            //logger.error("JWT claims string is empty - {}", ex.getMessage());
+        }
+        return false;
+    }
+}
