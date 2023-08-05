671a38482f12c30b72385585a66e25339e5a7e86
Ilya Dubenok <dubenokilya@gmail.com>
2023-08-05 22:20:23 +0300

    -added @EnableFeignClients to main class -added new filter to handle exceptions during jwt verification (when user_service is absent, for ex) -refactored SecurityConfig to remove warnings

--- audit-service/src/main/java/org/example/Main.java
+++ audit-service/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
--- audit-service/src/main/java/org/example/config/SecurityConfig.java
+++ audit-service/src/main/java/org/example/config/SecurityConfig.java
@@ -2,60 +2,55 @@ package org.example.config;
 
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.endpoint.web.filters.FilterChainExceptionFilter;
 import org.example.endpoint.web.filters.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter filter) throws Exception  {
-        // Enable CORS and disable CSRF
-        http = http.cors().and().csrf().disable();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter filter, FilterChainExceptionFilter filterChainExceptionFilter) throws Exception {
 
-        // Set session management to stateless
         http = http
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and();
-
-        // Set unauthorized requests exception handler
-        http = http
-                .exceptionHandling()
-                .authenticationEntryPoint(
-                        (request, response, ex) -> {
-                            response.setStatus(
-                                    HttpServletResponse.SC_UNAUTHORIZED
-                            );
-                        }
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagementConfigurer ->
+                        sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exceptionConfigurer ->
+                        exceptionConfigurer
+                                .authenticationEntryPoint(
+                                        (request, response, authException) ->
+                                                response.setStatus(
+                                                        HttpServletResponse.SC_UNAUTHORIZED
+                                                )
+                                )
+                                .accessDeniedHandler(
+                                        (request, response, accessDeniedException) ->
+                                                response.setStatus(HttpServletResponse.SC_FORBIDDEN)
+                                )
                 )
-                .accessDeniedHandler((request, response, ex) -> {
-                    response.setStatus(
-                            HttpServletResponse.SC_FORBIDDEN
-                    );
-                })
-                .and();
-
-        // Set permissions on endpoints
-        http.authorizeHttpRequests(requests -> requests
-                .requestMatchers("/api/v1/audit/**").hasAnyRole("ADMIN")
-                .anyRequest().authenticated()
-
-        );
-
-        // Add JWT token filter
-        http.addFilterBefore(
+                .authorizeHttpRequests(requests->requests
+                        .requestMatchers("/api/v1/audit/**").hasAnyRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(
                 filter,
-                UsernamePasswordAuthenticationFilter.class
-        );
+                UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(filterChainExceptionFilter,
+                        JwtFilter.class);
 
         return http.build();
     }
