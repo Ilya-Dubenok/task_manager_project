8ae07b3d88c78412be3066228d77b67d1f5518bb
Ilya Dubenok <dubenokilya@gmail.com>
2023-08-18 18:45:08 +0300

    removed unnecessary import

--- report-service/src/main/java/org/example/dao/api/IReportRepository.java
+++ report-service/src/main/java/org/example/dao/api/IReportRepository.java
@@ -5,8 +5,6 @@ import org.example.dao.entities.ReportStatus;
 import org.example.dao.entities.ReportType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
